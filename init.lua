93d415c262eb32aa6268575d5316bdffec2dfae2
Gerad Guzmán <geradd367@gmail.com>
2026-05-07 02:07:33 +0100

    Modified softtabstop
    Signed-off-by: Gerad Guzmán <geradd367@gmail.com>

--- init.lua
+++ init.lua
@@ -106,6 +106,7 @@ vim.o.relativenumber = true
 
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+vim.o.softtabstop = -1
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
